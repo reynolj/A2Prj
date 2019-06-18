f3dc0486c3ede7460c92e276d417441a40d25f1c
Jake Reynolds <Jake Reynolds@Classy-Lappy>
2019-06-18 13:56:21 -0700

    Added commands in Game Constructor

--- src/com/mycompany/a2/Game.java
+++ src/com/mycompany/a2/Game.java
@@ -1,10 +1,12 @@
 package com.mycompany.a2;
 
+import com.codename1.ui.Button;
 import com.codename1.ui.Form;
 import com.codename1.ui.Label;
 import com.codename1.ui.TextField;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
+import com.mycompany.a2.commands.*;
 
 public class Game extends Form {
 	private GameWorld gw;	
@@ -12,6 +14,104 @@ public class Game extends Form {
 	public Game() {
 		gw = GameWorld.getInstance();
 		gw.init();
+		
+		
+		//TODO: add aesthetics
+		Button addAsteroid		= 		new Button("Add Asteroid");
+		Button addNPS			= 		new Button("Add Enemy Ship");
+		Button addSpaceStation	=	 	new Button("Add Space Station");
+		Button addPlayerShip	=		new Button("Add Player Ship");
+		Button fireMissile		=		new Button("Fire Player Missile");
+		Button jump				=		new Button("Jump To Hyperspace");
+		//Add aesthetics below
+		
+		
+		//ALL COMMANDS
+		//Control Panel Buttons
+		AddAsteroidCommand myaddAsteroid = new AddAsteroidCommand(gw);
+		addAsteroid.setCommand( myaddAsteroid );
+		
+		AddNPSCommand myaddNPS = new AddNPSCommand(gw);
+		addNPS.setCommand(myaddNPS);
+		
+		AddSpaceStationCommand myaddST = new AddSpaceStationCommand(gw);
+		addNPS.setCommand(myaddST);
+		
+		AddPlayerShipCommand myaddPS = new AddPlayerShipCommand(gw);
+		addPlayerShip.setCommand(myaddPS);
+		
+		FirePSMissileCommand myFireMissile = new FirePSMissileCommand(gw);
+		fireMissile.setCommand(myFireMissile);
+		addKeyListener(-90, myFireMissile);
+		
+		JumpToHyperspaceCommand myJump = new JumpToHyperspaceCommand(gw);
+		jump.setCommand(myJump);
+		addKeyListener('j', myJump);
+		
+		//Key bindings
+		IncreasePSSpeedCommand myIncrease = new IncreasePSSpeedCommand(gw);
+		addKeyListener('i', myIncrease);
+		addKeyListener(-91, myIncrease);
+		
+		DecreasePSSpeedCommand myDecrease = new IncreasePSSpeedCommand(gw);
+		addKeyListener('d', myDecrease);
+		addKeyListener(-92, myDecrease);
+		
+		TurnPSLeftCommand myTurnLeft = new TurnPSLeftCommand(gw);
+		addKeyListener('l', myTurnLeft);
+		addKeyListener(-93, myTurnLeft);
+		
+		TurnPSRightCommand myTurnRight = new TurnPSRightCommand(gw);
+		addKeyListener('r', myTurnRight);
+		addKeyListener(-94, myTurnRight);
+		
+		TurnLauncherLeftCommand myTurnLauncherLeft = new TurnLauncherLeftCommand(gw);
+		addKeyListener('<', myTurnLauncherLeft);
+		
+		TurnLauncherRightCommand myTurnLauncherRight = new TurnLauncherRightCommand(gw);
+		addKeyListener('>', myTurnLauncherRight);
+		
+		LaunchNPSMissileCommand myLaunchMissile = new LaunchNPSMissileCommand(gw);
+		addKeyListener('L', myLaunchMissile);
+		
+		ReloadCommand myReload = new ReloadCommand(gw);
+		addKeyListener('n', myReload);
+		
+		KillAsteroidMissileCommand myKAM = new KillAsteroidMissileCommand(gw);
+		addKeyListener('k', myKAM);
+		
+		KillMissileNPS myKMN = new KillMissileNPS(gw);
+		addKeyListener('e', myKMN);
+		
+		Kill_NPS_Missile_PSCommand myKNMP = new Kill_NPS_Missile_PSCommand(gw);
+		addKeyListener('E', myKNMP);
+		
+		KillAsteroidPSCommand myKAP = new KillAsteroidPSCommand(gw);
+		addKeyListener('c', myKAP);
+		
+		Kill_NPS_PSCommand myKNP = new Kill_NPS_PSCommand(gw);
+		addKeyListener('h', myKNP);
+		
+		KillAsteroidAsteroidCommand myKAA = new KillAsteroidAsteroidCommand(gw);
+		addKeyListener('x', myKAA);
+		
+		KillAsteroidNPS myKAN = new KillAsteroidNPS(gw);
+		addKeyListener('I', myKAN);
+		
+		TickCommand myTick = new TickCommand(gw);
+		addKeyListener('t', myTick);
+		
+		QuitCommand myQuit = new QuitCommand(gw);
+		addKeyListener('Q', myQuit);
+		
+		/*
+		 * AboutCommand myAbout = new AboutCommand(gw);
+		 * SoundCommand mySound = new SoundCommand(gw);
+		 * NewCommand myNew = new NewCommand(gw);
+		 * SaveCommand mySave = new SaveCommand(gw);
+		 * UndoCommand myUndo = new UndoCommand(gw);
+		 */
+		//
 		play();
 	}
 	
