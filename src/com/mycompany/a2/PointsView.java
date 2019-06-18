6ee493c96b81840a7bf7d2ab8be182ce89ca5ee3
Jake Reynolds <Jake Reynolds@Classy-Lappy>
2019-06-18 15:52:28 -0700

    Pointsview

--- src/com/mycompany/a2/PointsView.java
+++ src/com/mycompany/a2/PointsView.java
@@ -3,14 +3,34 @@ package com.mycompany.a2;
 import java.util.Observable;
 import java.util.Observer;
 
+import com.codename1.charts.util.ColorUtil;
 import com.codename1.ui.Container;
+import com.codename1.ui.Label;
+import com.codename1.ui.layouts.BoxLayout;
 
 public class PointsView extends Container implements Observer {
-
+	private Label pointsValueLabel;
+	
+	public PointsView() {
+		Label pointsTextLabel = new Label("Points");
+		
+		pointsValueLabel = new Label("empty");
+		
+		pointsTextLabel.getAllStyles().setFgColor(ColorUtil.rgb(0,0,255));
+		
+		Container myContainer = new Container();
+		myContainer.setLayout(new BoxLayout(BoxLayout.X_AXIS));
+		
+		myContainer.add(pointsTextLabel);
+		this.add(myContainer);
+	}
+	
+	
 	@Override
 	public void update(Observable observable, Object data) {
-		// TODO Auto-generated method stub
+		IGameWorld gw = (IGameWorld) data;
+		this.pointsValueLabel.setText("" + gw.displayStats());
+		this.repaint();
 		
 	}
-
 }
