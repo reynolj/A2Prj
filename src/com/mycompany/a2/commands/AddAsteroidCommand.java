e28c64ec040cca69baa05b82cc4aca7dbeef8d8d
Jake Reynolds <Jake Reynolds@Classy-Lappy>
2019-06-18 12:44:52 -0700

    Added first half of commands

--- src/com/mycompany/a2/commands/AddAsteroidCommand.java
+++ src/com/mycompany/a2/commands/AddAsteroidCommand.java
@@ -4,4 +4,12 @@ import com.codename1.ui.Command;
 
 public class AddAsteroidCommand extends Command {
 
+	private Gameworld gw;
+	
+	public AddAsteroidCommand(Gameworld gw) {
+		super( "Add Asteroid");
+		this.gw = gw;
+		// TODO Auto-generated constructor stub
+	}
+
 }
---
+++ src/com/mycompany/a2/commands/DecreasePSSpeedCommand.java
@@ -0,0 +1,7 @@
+package com.mycompany.a2.commands;
+
+import com.codename1.ui.Command;
+
+public class DecreasePSSpeedCommand extends Command {
+
+}
