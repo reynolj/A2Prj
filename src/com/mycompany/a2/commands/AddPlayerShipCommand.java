ce1348968e4634a240a61f1f5cbd4d9343f208c7
SHendryx <samuelhendryx@csus.edu>
2019-06-18 13:30:46 -0700

    First 5 Commands

--- src/com/mycompany/a2/commands/AddPlayerShipCommand.java
+++ src/com/mycompany/a2/commands/AddPlayerShipCommand.java
@@ -1,7 +1,27 @@
 package com.mycompany.a2.commands;
 
 import com.codename1.ui.Command;
+import com.codename1.ui.events.ActionEvent;
+import com.mycompany.a2.GameWorld;
 
 public class AddPlayerShipCommand extends Command {
-
+	private GameWorld gw;
+	
+	/**
+	 * Constructor
+	 * @param gw GameWorld object
+	 */
+	public AddPlayerShipCommand(GameWorld gw) {
+		super("Add Player Ship");
+		this.gw = gw;
+	
+	}
+	
+	/**
+	 * Override action performed
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		gw.addPlayerShip();
+	}
 }
--- src/com/mycompany/a2/commands/AddSpaceStationCommand.java
+++ src/com/mycompany/a2/commands/AddSpaceStationCommand.java
@@ -1,7 +1,26 @@
 package com.mycompany.a2.commands;
 
 import com.codename1.ui.Command;
+import com.codename1.ui.events.ActionEvent;
+import com.mycompany.a2.GameWorld;
 
 public class AddSpaceStationCommand extends Command {
-
+	private GameWorld gw;
+	
+	/**
+	 * Constructor
+	 * @param gw GameWorld object
+	 */
+	public AddSpaceStationCommand(GameWorld gw) {
+		super("Add Space Station");
+		this.gw = gw;
+	}
+	
+	/**
+	 * Override action performed
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		gw.addSpaceStation();
+	}
 }
