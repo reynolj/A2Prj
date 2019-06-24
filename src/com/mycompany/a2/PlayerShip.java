47e8eac479fe51855faf2600c940bfba868f3826
SHendryx <samuelhendryx@csus.edu>
2019-06-24 12:26:16 -0700

    Re-factored PlayerShip. Organized functions.

--- src/com/mycompany/a2/PlayerShip.java
+++ src/com/mycompany/a2/PlayerShip.java
@@ -20,11 +20,6 @@ public class PlayerShip extends Ship implements ISteerable{
 		this.resetPlayerShip();
 	}
 	
-	public void jump() {
-		this.setLocation(CENTER_X, CENTER_Y);
-		this.steerableMissileLauncher.setLocation(CENTER_X, CENTER_Y);
-	}
-	
 	/**
 	 * This method is required to create an instance of PlayerShip.
 	 * If one already exists, returns the existing object.
@@ -35,7 +30,9 @@ public class PlayerShip extends Ship implements ISteerable{
 			PS = new PlayerShip();
 		return PS;
 	}
-
+	
+	///////////////////////////PlayerShip Helper Functions/////////////////////////
+	
 	/**
 	 * Returns a boolean to let us know if a PlayerShip exists or not.
 	 * @return True if PlayerShip doesn't exist, False if it does.
@@ -45,7 +42,16 @@ public class PlayerShip extends Ship implements ISteerable{
 	}
 	
 	/**
+	 * Removes a PlayerShip object from our object holder.
+	 * Useful for knowing whether or not a PlayerShip exists.
+	 */
+	public static void deleteShip() {
+		PS = null;
+	}
+	
+	/**
 	 * Resets the position of the player ship and its missile launcher to the center of the game board
+	 * This is primarily used when respawning the PlayerShip after it dies.
 	 * @param PS PlayerShip object
 	 */
 	public void resetPlayerShip() {
@@ -59,57 +65,16 @@ public class PlayerShip extends Ship implements ISteerable{
 	}
 	
 	/**
-	 * Removes a PlayerShip object from our object holder.
-	 * Useful for knowing whether or not a PlayerShip exists.
-	 */
-	public static void deleteShip() {
-		PS = null;
-	}
-	
-	
-	/**
 	 * Reloads the missile count for PlayerShip
 	 */
 	public void reloadMissiles() {
 		PS.setMissileCount(INIT_M_PS);
 	}
 	
-	/**
-	 * toString override method
-	 */
-	public String toString() {
-		String parentDesc = super.toString();
-		String localDesc = " Missile launcher dir = " + this.getLauncherDirection();
-		String header = "Player Ship: ";
-		
-		return header + parentDesc + localDesc;
-	}
+	///////////////////////////End PlayerShip Helper Functions/////////////////////
 	
-	/**
-	 * Returns direction of PlayerShip missile launcher
-	 * @return direction (int 0-359)
-	 */
-	public int getLauncherDirection() {
-		return PS.steerableMissileLauncher.getDirection();
-	}
+	///////////////////////////PlayerShip Movement Methods/////////////////////////
 	
-	/**
-	 * Turns missile launcher left.
-	 * Implements ISteerable
-	 */
-	public void turnLauncherLeft() {
-		PS.steerableMissileLauncher.turnLeft();
-	}
-
-	
-	/**
-	 * Turns missile launcher right.
-	 * Implements ISteerable
-	 */
-	public void turnLauncherRight() {
-		PS.steerableMissileLauncher.turnRight();
-	}
-
 	/**
 	 * Increases PS speed to current speed + SPEED_DELTA 
 	 */
@@ -137,7 +102,6 @@ public class PlayerShip extends Ship implements ISteerable{
 			return false;
 		}
 	}
-	
 
 	/**
 	 * Turning right (clockwise) is a increase in the degree angle according to the specification.
@@ -163,6 +127,37 @@ public class PlayerShip extends Ship implements ISteerable{
 		this.steerableMissileLauncher.turnLeft();
 	}
 	
+	/**
+	 * Jumps ship to hyperspace. PlayerShip's location resets to the center of the GameWorld.
+	 * The SteerableMissileLauncher's location is also updated.
+	 * 
+	 * This method is different from ResetPlayerShip in that it does not affect the speed
+	 * of the PlayerShip or Launcher.
+	 */
+	public void jump() {
+		this.setLocation(CENTER_X, CENTER_Y);
+		this.steerableMissileLauncher.setLocation(CENTER_X, CENTER_Y);
+	}
+	
+	///////////////////////////End PlayerShip Movement Methods/////////////////////
+
+	////////////SteerableMissileLauncher Movement and Helper Methods///////////////
+	
+	/**
+	 * Turns missile launcher left.
+	 * Implements ISteerable
+	 */
+	public void turnLauncherLeft() {
+		PS.steerableMissileLauncher.turnLeft();
+	}
+	
+	/**
+	 * Turns missile launcher right.
+	 * Implements ISteerable
+	 */
+	public void turnLauncherRight() {
+		PS.steerableMissileLauncher.turnRight();
+	}
 
 	/**
 	 * Sets the SteerableMissileLauncher's new location after move.
@@ -172,9 +167,24 @@ public class PlayerShip extends Ship implements ISteerable{
 	}
 	
 	/**
-	 * Sets the SteerableMissileLauncher's speed after changing PS speed.
+	 * Returns direction of PlayerShip missile launcher
+	 * @return direction (int 0-359)
 	 */
-	public void setMLSpeed() {
-		steerableMissileLauncher.setSpeed(this.getSpeed());
+	public int getLauncherDirection() {
+		return PS.steerableMissileLauncher.getDirection();
 	}
+	
+	////////////End SteerableMissileLauncher Movement and Helper Methods////////////
+	
+	/**
+	 * toString override method
+	 */
+	public String toString() {
+		String parentDesc = super.toString();
+		String localDesc = " Missile launcher dir = " + this.getLauncherDirection();
+		String header = "Player Ship: ";
+		
+		return header + parentDesc + localDesc;
+	}
+	
 }
